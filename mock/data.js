862387e0bc904fb6f94a192716eb544f777f9179
mack-feng <962116239@qq.com>
2020-03-27 23:57:55 +0800

    文章内容页面

---
+++ mock/data.js
@@ -0,0 +1,251 @@
+export const newsList = [
+    {
+        title: '从上海到泸溪兴隆场，她的蜂蜜刷屏了朋友圈！',
+        author: '玉伯',
+        type: 'gz',
+        read: 125,
+        time: '10分钟前'
+    },
+    {
+        title: '逆向奔跑者陈艳玲：返乡创业打造新邵农业生态新名片',
+        author: '艳玲',
+        imgLen: 3,
+        type: 'tj',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/03/18/906f8c9ffd294bd1904440c7c5077e34.png',
+        read: 9525,
+        time: '1小时前'
+    },
+    {
+        title: '农村6旬老农山林种砂仁，通过卖砂仁卖苗年挣40万！',
+        author: '老农',
+        imgLen: 1,
+        type: 'tj',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/02/19/7152c9f846b34188bd09def5cdbe878e.png',
+        read: 65253,
+        time: '2小时前'
+    },
+    {
+        title: '他专心养鱼，第一年就卖了18万元！',
+        author: '虫鸣',
+        type: 'fp',
+        read: 25253,
+        time: '3小时前'
+    },
+    {
+        title: '这位书记不简单，他用一颗小小葡萄撑起了一条产业链！',
+        author: '皓诚',
+        imgLen: 1,
+        type: 'fp',
+        img: 'https://image.cnhnb.com/image/jpg/miniapp/2020/03/25/066ec18210bb4c718515266a641b8e59.jpg',
+        read: 56894,
+        time: '4小时前'
+    },
+    {
+        title: '特别推荐丨从“门外汉”到电商能人，她奏响了电商致富曲',
+        author: '玉伯',
+        imgLen: 3,
+        type: 'gz',
+        type: 'zc',
+        img: 'https://image.cnhnb.com/image/jpg/miniapp/2020/01/13/29882ea72671404cb97bdae1918a97cf.jpg',
+        read: 125,
+        time: '1天前'
+    },
+    {
+        title: '农村6旬老农山林种砂仁，通过卖砂仁卖苗年挣40万！',
+        author: '邵恒',
+        type: 'js',
+        read: 562,
+        time: '2小时前'
+    },
+    {
+        title: '逆向奔跑者陈艳玲：返乡创业打造新邵农业生态新名片',
+        author: '艳玲',
+        imgLen: 1,
+        type: 'js',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/03/18/906f8c9ffd294bd1904440c7c5077e34.png',
+        read: 136,
+        time: '8小时前'
+    },
+    {
+        title: '他专心养鱼，第一年就卖了18万元！',
+        author: '马越',
+        read: 956,
+        type: 'rd',
+        time: '5小时前'
+    },
+    {
+        title: '农村6旬老农山林种砂仁，通过卖砂仁卖苗年挣40万！',
+        author: '黄磊',
+        imgLen: 3,
+        type: 'gz',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/02/19/7152c9f846b34188bd09def5cdbe878e.png',
+        read: 69853,
+        time: '16小时前'
+    },
+    {
+        title: '从上海到泸溪兴隆场，她的蜂蜜刷屏了朋友圈！',
+        author: '玉伯',
+        type: 'gz',
+        read: 125,
+        time: '10分钟前'
+    },
+    {
+        title: '逆向奔跑者陈艳玲：返乡创业打造新邵农业生态新名片',
+        author: '艳玲',
+        imgLen: 3,
+        type: 'tj',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/03/18/906f8c9ffd294bd1904440c7c5077e34.png',
+        read: 9525,
+        time: '1小时前'
+    },
+    {
+        title: '农村6旬老农山林种砂仁，通过卖砂仁卖苗年挣40万！',
+        author: '老农',
+        imgLen: 1,
+        type: 'tj',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/02/19/7152c9f846b34188bd09def5cdbe878e.png',
+        read: 65253,
+        time: '2小时前'
+    },
+    {
+        title: '他专心养鱼，第一年就卖了18万元！',
+        author: '虫鸣',
+        type: 'fp',
+        read: 25253,
+        time: '3小时前'
+    },
+    {
+        title: '这位书记不简单，他用一颗小小葡萄撑起了一条产业链！',
+        author: '皓诚',
+        imgLen: 1,
+        type: 'fp',
+        img: 'https://image.cnhnb.com/image/jpg/miniapp/2020/03/25/066ec18210bb4c718515266a641b8e59.jpg',
+        read: 56894,
+        time: '4小时前'
+    },
+    {
+        title: '特别推荐丨从“门外汉”到电商能人，她奏响了电商致富曲',
+        author: '玉伯',
+        imgLen: 3,
+        type: 'gz',
+        type: 'zc',
+        img: 'https://image.cnhnb.com/image/jpg/miniapp/2020/01/13/29882ea72671404cb97bdae1918a97cf.jpg',
+        read: 125,
+        time: '1天前'
+    },
+    {
+        title: '农村6旬老农山林种砂仁，通过卖砂仁卖苗年挣40万！',
+        author: '邵恒',
+        type: 'js',
+        read: 562,
+        time: '2小时前'
+    },
+    {
+        title: '逆向奔跑者陈艳玲：返乡创业打造新邵农业生态新名片',
+        author: '艳玲',
+        imgLen: 1,
+        type: 'js',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/03/18/906f8c9ffd294bd1904440c7c5077e34.png',
+        read: 136,
+        time: '8小时前'
+    },
+    {
+        title: '他专心养鱼，第一年就卖了18万元！',
+        author: '马越',
+        read: 956,
+        type: 'rd',
+        time: '5小时前'
+    },
+    {
+        title: '农村6旬老农山林种砂仁，通过卖砂仁卖苗年挣40万！',
+        author: '黄磊',
+        imgLen: 3,
+        type: 'gz',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/02/19/7152c9f846b34188bd09def5cdbe878e.png',
+        read: 69853,
+        time: '16小时前'
+    },
+    {
+        title: '从上海到泸溪兴隆场，她的蜂蜜刷屏了朋友圈！',
+        author: '玉伯',
+        type: 'gz',
+        read: 125,
+        time: '10分钟前'
+    },
+    {
+        title: '逆向奔跑者陈艳玲：返乡创业打造新邵农业生态新名片',
+        author: '艳玲',
+        imgLen: 3,
+        type: 'tj',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/03/18/906f8c9ffd294bd1904440c7c5077e34.png',
+        read: 9525,
+        time: '1小时前'
+    },
+    {
+        title: '农村6旬老农山林种砂仁，通过卖砂仁卖苗年挣40万！',
+        author: '老农',
+        imgLen: 1,
+        type: 'tj',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/02/19/7152c9f846b34188bd09def5cdbe878e.png',
+        read: 65253,
+        time: '2小时前'
+    },
+    {
+        title: '他专心养鱼，第一年就卖了18万元！',
+        author: '虫鸣',
+        type: 'fp',
+        read: 25253,
+        time: '3小时前'
+    },
+    {
+        title: '这位书记不简单，他用一颗小小葡萄撑起了一条产业链！',
+        author: '皓诚',
+        imgLen: 1,
+        type: 'fp',
+        img: 'https://image.cnhnb.com/image/jpg/miniapp/2020/03/25/066ec18210bb4c718515266a641b8e59.jpg',
+        read: 56894,
+        time: '4小时前'
+    },
+    {
+        title: '特别推荐丨从“门外汉”到电商能人，她奏响了电商致富曲',
+        author: '玉伯',
+        imgLen: 3,
+        type: 'gz',
+        type: 'zc',
+        img: 'https://image.cnhnb.com/image/jpg/miniapp/2020/01/13/29882ea72671404cb97bdae1918a97cf.jpg',
+        read: 125,
+        time: '1天前'
+    },
+    {
+        title: '农村6旬老农山林种砂仁，通过卖砂仁卖苗年挣40万！',
+        author: '邵恒',
+        type: 'js',
+        read: 562,
+        time: '2小时前'
+    },
+    {
+        title: '逆向奔跑者陈艳玲：返乡创业打造新邵农业生态新名片',
+        author: '艳玲',
+        imgLen: 1,
+        type: 'js',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/03/18/906f8c9ffd294bd1904440c7c5077e34.png',
+        read: 136,
+        time: '8小时前'
+    },
+    {
+        title: '他专心养鱼，第一年就卖了18万元！',
+        author: '马越',
+        read: 956,
+        type: 'rd',
+        time: '5小时前'
+    },
+    {
+        title: '农村6旬老农山林种砂仁，通过卖砂仁卖苗年挣40万！',
+        author: '黄磊',
+        imgLen: 3,
+        type: 'gz',
+        img: 'https://image.cnhnb.com/image/png/miniapp/2020/02/19/7152c9f846b34188bd09def5cdbe878e.png',
+        read: 69853,
+        time: '16小时前'
+    }
+]
