e433abbd7f09df53360a895112aa8048db31ba84
mack-feng <962116239@qq.com>
2020-03-28 23:09:29 +0800

    微信分享

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    show: false
   },
 
   /**
@@ -16,51 +16,20 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 关闭弹出层
    */
-  onReady: function () {
-
+  onClose(){
+    this.setData({
+      show: false
+    })
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 显示分享弹出层
    */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  showShare(){
+    this.setData({
+      show: true
+    })
   }
 })
